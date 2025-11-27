7d1a49305ee5b032bbd2aef20d8c6cd6dee12cad
chenhg5 <chenhonggui@wps.cn>
2025-11-27 17:50:34 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Layout from './components/Layout';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { DarkModeProvider } from './contexts/DarkModeContext';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 function App() {
   return (
     <DarkModeProvider>
       <LanguageProvider>
+        <GoogleAnalytics />
         <Layout />
       </LanguageProvider>
     </DarkModeProvider>
